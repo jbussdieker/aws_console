af7ec2fbd010ef944cee2b460021c7973567e5f8
Joshua Bussdieker <jbussdieker@gmail.com>
2013-02-27 19:28:38 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwsConsole::Application.config.secret_token = 'c68d94d2e3e4bb051eae366e5d9846d16b338a47e0ae8aa76d0935b49712aa4279fb042832659a2e8c6c0740fbd46f3aa0a510db1f9530525f2593dcbd5aa9c4'
